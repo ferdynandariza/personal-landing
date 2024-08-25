f712eff90875451baa9a6d1505f6b83c6423e6d0
Ferdy Nanda Riza <waka8a@gmail.com>
2024-08-24 23:06:14 -0700

    feat: add home component

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fnr-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+})
+export class HomeComponent {}
